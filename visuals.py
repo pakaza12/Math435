aeb95e97557746a5bd8c6c089222af787443bd97
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-10-19 15:14:16 -0500

    pair plots

--- visuals.py
+++ visuals.py
@@ -6,6 +6,12 @@ import xlrd
 
 sales_data = pd.read_excel("MitC2022data - SalesPopulation.xlsx")
 valid_sales = sales_data[sales_data["SaleValidity"] == 1]
-print(len(valid_sales["SaleValidity"]))
+#print(len(valid_sales["SaleValidity"]))
 
+#plt.scatter(x=valid_sales["TLA"], y=valid_sales["Price"], title="Price vs. Total Living Area")
+#plt.show()
+
+valid_sales_numeric = valid_sales[["CDU", "Qual", "TLA", "YrBlt", "LandValue", "Price"]]
+sns.pairplot(valid_sales_numeric)
+plt.show()
 
