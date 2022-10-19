74d180b254581fa73d26d9a6cb61ddf86ad910ab
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-10-19 14:50:12 -0500

    subset data
    limiting data to valid sales

--- visuals.py
+++ visuals.py
@@ -3,3 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import xlrd
+
+sales_data = pd.read_excel("MitC2022data - SalesPopulation.xlsx")
+valid_sales = sales_data[sales_data["SaleValidity"] == 1]
+print(len(valid_sales["SaleValidity"]))
+
+
