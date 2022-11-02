282760c4cdf5833c7ee02953a38c2b7f8d9a4790
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-11-02 13:56:26 -0500

    adding histogram

--- visuals.py
+++ visuals.py
@@ -6,14 +6,42 @@ import seaborn as sns
 import xlrd
 import scipy
 
-sales_data = pd.read_excel("MitC2022data - SalesPopulation.xlsx")
-valid_sales = sales_data[sales_data["SaleValidity"] == 1]
+sales_data = pd.read_excel("extraFilter.xlsx")
 
-plt.scatter(x=valid_sales["LandValue"], y=valid_sales["Price"], color="purple")
+""" plt.scatter(x=valid_sales["LandValue"], y=valid_sales["Price"], color="purple")
 plt.title("Price vs. Land Value")
 plt.ylabel("Price (in millions)")
 plt.xlabel("Land Value")
+plt.show() """
+
+valid_price = sales_data["Price"]
+sd = np.std(valid_price)
+mn = np.mean(valid_price)
+# normal
+a_mu, a_std = scipy.stats.norm.fit(valid_price)
+xs = np.linspace(0, max(valid_price))
+p = scipy.stats.norm.pdf(xs, a_mu, a_std)
+
+
+
+sns.displot(data=sales_data, x='Price', kde=True, bins=50, color = "green")
+plt.axvline(x=mn+(2*sd),color="steelblue", linestyle="dashed")
+plt.axvline(x=0,color="steelblue", linestyle="dashed")
+
+plt.xlabel('Price (in millions)')
+plt.ylabel('Number of homes')
+plt.title('Price Distribution')
 plt.show()
 
-valid_price = valid_sales["Price"]
+""" plt.hist(valid_price, bins=30, color = "green", edgecolor="black")
+
+plt.plot(xs, p)
+#plt.xlim(0, 1.5e6)
+
+plt.show() """
+
+
+
+
+
 
